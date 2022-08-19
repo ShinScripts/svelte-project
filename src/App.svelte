bfc4fa0f21bbc0003ceea2b1c08c65b2c5f9fccf
Shincode <shahin02@live.se>
2022-08-19 20:56:34 +0200

    init

---
+++ src/App.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import Counter from './lib/Counter.svelte'
+</script>
+
+<main class="bg-zinc-600 w-[100vw] h-[100vh] flex justify-center items-center">
+  <Counter />
+</main>
+
