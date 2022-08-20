87704597f7f8c92440fb6fd793043ae6b2c98b58
Shincode <shahin02@live.se>
2022-08-20 03:09:19 +0200

    small changes to xbox on main page

--- src/lib/components/MainXbox.svelte
+++ src/lib/components/MainXbox.svelte
@@ -4,10 +4,10 @@
 	export let description: string[];
 </script>
 
-<div>
+<div class="flex justify-center items-center flex-col mx-10 hover:bg-black">
 	<img src={image} alt="xbox" class="w-[600px] h-[600px]">
-	<h1 class="text-white underline text-4xl">{title}</h1>
+	<h1 class="text-white underline text-4xl py-10 text-center">{title}</h1>
 	{#each description as text}
-		<p class="text-white">✓ {text}</p>
+		<p class="text-white py-1">✓ {text}</p>
 	{/each}
 </div>
